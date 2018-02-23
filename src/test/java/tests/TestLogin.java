f265d88e353c414988c4350b91b7b194f349cfbd
Ibne Zabed <ibnezabed@Ibnes-MacBook-Pro.local>
2018-02-22 21:02:05 -0500

    Added assertion along with thread.sleep

--- src/test/java/tests/TestLogin.java
+++ src/test/java/tests/TestLogin.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import static org.junit.Assert.*;
+
 public class TestLogin {
 
     private WebDriver driver;
@@ -18,11 +20,14 @@ public class TestLogin {
     }
 
     @Test
-    public void succeeded() {
+    public void succeeded() throws InterruptedException {
         driver.get("http://the-internet.herokuapp.com/login");
         driver.findElement(By.id("username")).sendKeys("tomsmith");
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.id("login")).submit();
+        Thread.sleep(2000);
+        assertTrue("success message not present",
+                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
     }
 
     @After
