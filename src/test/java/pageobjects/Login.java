827f9349b82308634af76e6e1f380a90c477592e
Ibne Zabed <ibnezabed@Ibnes-MacBook-Pro.local>
2018-02-22 22:05:10 -0500

    Create a Base - facade layer w/ common selenium methods

--- src/test/java/pageobjects/Login.java
+++ src/test/java/pageobjects/Login.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertTrue;
 
-public class Login {
+public class Login extends Base {
 
     private WebDriver driver;
     By usernameLocator = By.id("username");
@@ -15,25 +15,28 @@ public class Login {
     By failureMessageLocator = By.cssSelector(".flash.error");
 
     public Login(WebDriver driver) {
-        this.driver = driver;
-        driver.get("http://the-internet.herokuapp.com/login");
+        super(driver);
+        visit("http://the-internet.herokuapp.com/login");
         // assertions are typically in the tests folder, exception to rule
         // Makes sure the selenium is in the right place before proceeding
         // Ensures that the tests will only proceed if the login form is present
-        // assertTrue("The login form is not present", isDisplayed(loginFormLocator));
+        assertTrue("The login form is not present", isDisplayed(loginFormLocator));
     }
 
     public void with(String username, String password) {
-        driver.findElement(usernameLocator).sendKeys(username);
-        driver.findElement(passwordLocator).sendKeys(password);
-        driver.findElement(loginFormLocator).submit();
+        //driver.findElement(usernameLocator).sendKeys(username);
+        type(username, usernameLocator);
+        //driver.findElement(passwordLocator).sendKeys(password);
+        type(password, passwordLocator);
+        //driver.findElement(loginFormLocator).submit();
+        submit(loginFormLocator);
     }
 
     public Boolean successMessagePresent() {
-        return driver.findElement(successMessageLocator).isDisplayed();
+        return isDisplayed(successMessageLocator);
     }
 
     public Boolean failureMessagePresent() {
-        return driver.findElement(failureMessageLocator).isDisplayed();
+        return isDisplayed(failureMessageLocator);
     }
 }
