52f9812592a018b2cd411c7cb520e226cb76f0c6
Ibne Zabed <ibnezabed@Ibnes-MBP.T-mobile.com>
2018-03-01 20:24:46 -0500

    Saucelab config

--- src/test/java/tests/Base.java
+++ src/test/java/tests/Base.java
@@ -5,22 +5,36 @@ import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 import static tests.Config.browser;
 
-public class Base {
+public class Base implements Config {
     protected WebDriver driver;
 
     @Rule
     public ExternalResource resource = new ExternalResource() {
         @Override
         protected void before() throws Throwable {
-            if (browser.equals("firefox")) {
-                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver");
-                driver = new FirefoxDriver();
-            } else if (browser.equals("chrome")) {
-                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver");
-                driver = new ChromeDriver();
+            if (host.equals("saucelabs")) {
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setCapability("browserName", browser);
+                capabilities.setCapability("version", browserVersion);
+                capabilities.setCapability("platform", platform);
+                String sauceUrl = String.format("http://%s:%s@ondemand.saucelabs.com:80/wd/hub", sauceUser, sauceKey);
+                driver = new RemoteWebDriver(new URL(sauceUrl), capabilities);
+            } else if (host.equals("localhost")) {
+                // Browsers
+                if (browser.equals("firefox")) {
+                    System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver");
+                    driver = new FirefoxDriver();
+                } else if (browser.equals("chrome")) {
+                    System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/vendor/chromedriver");
+                    driver = new ChromeDriver();
+                }
             }
         }
 
--- src/test/java/tests/Config.java
+++ src/test/java/tests/Config.java
@@ -3,4 +3,9 @@ package tests;
 public interface Config {
     final String baseUrl = System.getProperty("baseUrl", "http://the-internet.herokuapp.com");
     final String browser = System.getProperty("browser", "firefox");
+    final String host = System.getProperty("host", "localhost");
+    final String browserVersion = System.getProperty("browserVersion", "33");
+    final String platform = System.getProperty("platform", "Windows XP");
+    final String sauceUser = "deans32";
+    final String sauceKey = "testpass32";
 }
